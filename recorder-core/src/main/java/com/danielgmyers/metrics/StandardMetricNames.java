43ec9f05f6fccd15e1add68d85483bf467c20467
Dan Myers <dan@danielgmyers.com>
2022-11-30 22:37:52 -0800

    Update copyright line in source files to say "Metric Recorder Contributors" instead of "Flux Contributors".

--- recorder-core/src/main/java/com/danielgmyers/metrics/StandardMetricNames.java
+++ recorder-core/src/main/java/com/danielgmyers/metrics/StandardMetricNames.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright Flux Contributors
+ *   Copyright Metric Recorder Contributors
  *
  *   Licensed under the Apache License, Version 2.0 (the "License").
  *   You may not use this file except in compliance with the License.
